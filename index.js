e0851bb20f4d0863d953517bf45bfb2b747e2a67
MarceloArraes <marcelo.arraes@gmail.com>
2022-07-17 22:30:30 +0000

    feat: now the stream of objets is correct by line with values.

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 const { parse } = require('csv-parse');
+const fs = require('fs');
 
+const results = [];
+
+fs.createReadStream('kepler_data.csv')
+  .pipe(parse({
+    comment: '#',
+    columns: true,
+    delimiter: ','
+  }))
+  .on('data', (data) => {
+    results.push(data);
+  })
+  .on('error', (err) => {
+    console.log(err);
+  })
+  .on('end', () => {
+    console.log(results);
+    console.log('done');
+  }
+);
