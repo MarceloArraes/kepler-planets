8cc8a7cb68f47defe28ce8facae780f74da5be8c
MarceloArraes <marcelo.arraes@gmail.com>
2022-07-17 22:36:38 +0000

    feat: adding more criteria to habitable planets.

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const fs = require('fs');
 
 const results = [];
 
+function isHabitablePlanet(planet) {
+  return planet['koi_disposition'] === 'CONFIRMED'
+    && planet['koi_insol'] > 0.36
+    && planet['koi_insol'] < 1.11
+    && planet['koi_prad'] < 1.6;
+}
+
+
 fs.createReadStream('kepler_data.csv')
   .pipe(parse({
     comment: '#',
@@ -10,13 +18,16 @@ fs.createReadStream('kepler_data.csv')
     delimiter: ','
   }))
   .on('data', (data) => {
-    results.push(data);
+    if (isHabitablePlanet(data)) {
+      results.push(data);
+    }
   })
   .on('error', (err) => {
     console.log(err);
   })
   .on('end', () => {
     console.log(results);
+    console.log(`There are ${results.length} habitable planets.`);
     console.log('done');
   }
 );
